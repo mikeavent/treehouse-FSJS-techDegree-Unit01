2f1936f3f2bd96165ac827b643778ef9ac264e8b
mikeavent <hello@mikeavent.me>
2019-03-17 16:35:33 -0400

    moved newQuote inside function to get the button to work

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ project 1 - A Random Quote Generator
 
 // quotes can be found in js/quotes.js
 
-var newQuote;
 
 /*
 function creates a random number from 0 - 9 and returns
@@ -19,29 +18,15 @@ function getRandomQuote() {
   }
 
 //console.log(getRandomQuote());
-// new Quote object from getRandomQuote function
-newQuote = getRandomQuote();
-/***
-  Create the `printQuote` function to:
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string.
-***/
-
 
 /*
 function which creates DOM Elements with the random quote, source and if
 available the citation and year which are use a conditional statement which
 bases it's decision on whether or not it has any characters.
 */
+
 function printQuote() {
-  var html;
+  var newQuote = getRandomQuote();
   var element = document.getElementById("quote-box").getElementsByClassName("quote")[0];
   element.innerHTML = newQuote.quote;
   var element = document.getElementById("quote-box").getElementsByClassName("source")[0];
