5890928cb18e95fe8667a549236c15ea349d928e
mikeavent <hello@mikeavent.me>
2019-03-17 01:15:37 -0400

    initial commit, project mostly done

---
+++ js/quotes.js
@@ -0,0 +1,62 @@
+var quotes = [
+  {
+    quote: "Game over, man. Game over!",
+    source: "Private William Hudson",
+    citation: "Aliens",
+    year: "1986"
+  },
+  {
+    quote: "It was the best of times it was the blurst of times.",
+    source: "Smoking Monkey",
+    citation: "The Simpsons",
+    year: "1993"
+  },
+  {
+    quote: "Hey man your clock wont flush.",
+    source: "Charlie",
+    citation: "Family Guy",
+    year: "1999"
+  },
+  {
+    quote: "Im Melting! Melting!",
+    source: "Wicked Witch",
+    citation: "Wizard of Oz",
+    year: "1939"
+  },
+  {
+    quote: " Oh my God they killed Kenny!",
+    source: "Stan Marsh",
+    citation: "South Park",
+    year: ""
+  },
+  {
+    quote: "Come again for Big Fudge?",
+    source: "Marshall Eriksen",
+    citation: "How I Met Your Mother",
+    year: ""
+  },
+  {
+    quote: "No problemo.",
+    source: "The Terminator",
+    citation: "Terminator 2: Judgement Day",
+    year: "1991"
+  },
+  {
+    quote: "P.A.R.T. Why?  Because I gotta!",
+    source: "Stanley Ipkiss as the Mask",
+    citation: "The Mask",
+    year: "1994"
+  },
+  {
+    quote: "Let's turn on the juice and see what shakes loose.",
+    source: "Beetlejuice",
+    citation: "Beetlejuice",
+    year: "1988"
+  },
+  {
+    quote: "Hey sexy mama. Wanna kill all humans?",
+    source: "Bender",
+    citation: "Futurama",
+    year: "1999"
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,71 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// quotes can be found in js/quotes.js
+
+
+/*
+function creates a random number from 0 - 9 and returns
+an object assosiated with the index number of the array is randomaly chosen
+the object is returned
+*/
+function getRandomQuote() {
+  var randomNum = Math.floor(Math.random() * 10);
+  randomQuote = quotes[randomNum];
+  //console.log(randomQuote);
+  return randomQuote
+  }
+
+//console.log(getRandomQuote());
+// new Quote object from getRandomQuote function
+var newQuote = getRandomQuote();
+/***
+  Create the `printQuote` function to:
+   - Call the `getRandomQuote` function and assign it to a variable.
+   - Create a variable for the HTML string and set it equal to an empty string.
+   - Use the HTML template in the instructions or the markup in the index.html file, AND
+     the random quote vairable to build your HTML string.
+   - Add the quote and source section to the HTML string.
+   - Use an if statement to check for the citation property before adding it to the HTML string.
+   - Use an if statement to check for the year property before adding it to the HTML string.
+   - Don't forget to close that final `p` tag.
+   - Set the `innerHTML` of the `quote-box` div to the HTML string.
+***/
+
+
+/*
+function which creates DOM Elements with the random quote, source and if
+available the citation and year which are use a conditional statement which
+bases it's decision on whether or not it has any characters.
+*/
+function printQuote() {
+  var element = document.getElementById("quote-box").getElementsByClassName("quote")[0];
+  element.innerHTML = newQuote.quote;
+  var element = document.getElementById("quote-box").getElementsByClassName("source")[0];
+  element.innerHTML = newQuote.source;
+    if (newQuote.citation.length > 0) {
+      var span1 = document.createElement('span');
+      var insidePara = document.getElementById("quote-box").getElementsByTagName("p")[1];
+      insidePara.appendChild(span1).setAttribute("class", "citation");
+      element = document.getElementsByTagName("span")[0];
+      element.innerText = newQuote.citation;
+    }
+    if (newQuote.citation.length > 0) {
+      var span2 = document.createElement('span');
+      insidePara.appendChild(span2).setAttribute("class", "year");
+      element = document.getElementsByTagName("span")[1];
+      element.innerText = newQuote.year;
+    }
+}
+
+printQuote();
+/***
+  When the "Show another quote" button is clicked, the event listener
+  below will be triggered, and it will call, or "invoke", the `printQuote`
+  function. So do not make any changes to the line of code below this
+  comment.
+***/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
